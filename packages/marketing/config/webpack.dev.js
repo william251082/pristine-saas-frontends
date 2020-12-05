9cdbd2980ffe4bd9322930f5c4de054db60086f5
william <william.del.rosario@iceshop.nl>
2020-12-05 21:13:28 +0100

    Production Webpack Config for Marketing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
@@ -21,9 +20,6 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
-        }),
-        new HtmlWebPackPlugin({
-            template: './public/index.html'
         })
     ],
     resolve: {
