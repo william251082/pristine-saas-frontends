1601c3e7bea19369d5b7320a150b582e3b24fa20
william <william.del.rosario@iceshop.nl>
2020-12-07 17:15:16 +0100

    Dashboard -- Integration with the Container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -22,7 +22,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js'
+                auth: 'auth@http://localhost:8082/remoteEntry.js',
+                dashboard: 'dashboard@http://localhost:8083/remoteEntry.js'
             },
             shared: packageJson.dependencies
         })
