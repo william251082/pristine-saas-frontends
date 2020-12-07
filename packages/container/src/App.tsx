765eb8ad6bb242383b54d64f0c0cac3864f407b5
william <william.del.rosario@iceshop.nl>
2020-12-07 12:30:34 +0100

    Auth --Integrating Auth into the Container (bug: has to press login twice to work)

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import AuthApp from "./components/AuthApp";
 import Header from "./components/Header";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 const generateClassName = createGenerateClassName({
@@ -14,6 +15,10 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header/>
+                        <Switch>
+                            <Route path="/auth" component={AuthApp} />
+                            <Route path="/" component={MarketingApp} />
+                        </Switch>
                     <MarketingApp/>
                 </div>
             </StylesProvider>
---
+++ packages/container/src/components/AuthApp.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useRef} from "react";
+import {mount} from "auth/AuthApp";
+import {useHistory} from "react-router-dom";
+
+export default () => {
+    const ref = useRef(null);
+    const history = useHistory();
+
+    useEffect(() => {
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        history.listen(onParentNavigate)
+    }, []);
+
+    return <div ref={ref}/>
+};
